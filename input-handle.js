b6a2420207ba3ef8087aa43de9a882605f4ea760
santosh072k <santosh072k@gmail.com>
2022-03-25 19:29:03 +0530

    added data urls

--- input-handle.js
+++ input-handle.js
@@ -2,15 +2,21 @@ const fileInput = document.getElementById("fileInput");
 
 const FILE_KEY = "FILE";
 
-function handleInput(evt){
-  const file = this.files[0];
-  const objectURL = URL.createObjectURL(file)
-  localStorage.setItem(FILE_KEY, objectURL);
-  
+function handleInput(evt) {
+    const file = this.files[0];
 
+    const reader = new FileReader();
+    reader.addEventListener(
+        "load",
+        function () {
+            localStorage.setItem(FILE_KEY, reader.result);
+        },
+        false
+    );
 
+    if (file) {
+        reader.readAsDataURL(file);
+    }
 }
 
-
 fileInput.addEventListener("change", handleInput);
-
